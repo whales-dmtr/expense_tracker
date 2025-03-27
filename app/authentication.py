3e5e88076f4ecbbc8631e9df57b0b6f97248bbb9
whales-dmtr <dmytrooliinyk2803@gmail.com>
2025-03-27 22:57:42 +0200

    Fix database leak

--- app/authentication.py
+++ app/authentication.py
@@ -5,16 +5,17 @@ import psycopg2
 import os
 
 auth_router = APIRouter()
-load_dotenv()
+load_dotenv()   
 
 
 @auth_router.get('/login')
 def login(user: LoginValidation) -> dict[str, str]:    
     conn = psycopg2.connect(
-        dbname='expense_tracker',
-        user='whale',
-        password='whale2803',
-        port='5432'
+        dbname=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT'),
     )
     
     cur = conn.cursor()
